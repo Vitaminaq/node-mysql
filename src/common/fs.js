fe0bf34e85fa356d474ca86e108833c416497e1e
肖鹏飞 <xiaopengfei@followme-inc.com>
2018-11-15 02:31:37 +0800

    feat: 区分上传图片格式保存
    区分上传图片格式保存

--- src/common/fs.js
+++ src/common/fs.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 
 const saveHeaderImg = (data, nickname) => {
     return new Promise((resolve, reject) => {
-        const path = `./src/public/images/${nickname}.jpg`;//从app.js级开始找--在我的项目工程里是这样的
+        const type = data.split(';')[0].split('image/')[1];
+        const path = `./src/public/images/${nickname}.${type}`;//从app.js级开始找--在我的项目工程里是这样的
         const base64 = data.replace(/^data:image\/\w+;base64,/, "");//去掉图片base64码前面部分data:image/png;base64
         const dataBuffer = new Buffer(base64, 'base64'); //把base64码转成buffer对象，
         console.log('dataBuffer是否是Buffer对象：'+Buffer.isBuffer(dataBuffer));
